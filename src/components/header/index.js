ae3eeb83a5fce04cb602e28acf78a15e12a418d1
Jaycee Go <itsmejcgo@gmail.com>
2023-05-01 22:10:37 +0800

    fixes media queries for max width each component

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+function index() {
+  return (
+    <div className="header">
+
+    </div>
+  )
+}
+
+export default index
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -15,26 +15,24 @@ return (
 
 const Navbar = ({ items }) => {
 return (
-    <header>
-        <nav className='xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md text-[10pt] px-4 mx-auto'>
-        <ul className='flex items-center'>
-            <li className='px-4 transition-colors duration-200 cursor-pointer place-items-center hover:bg-primary-red'>
-                <a href='https://www.gov.ph/' className='py-[3px]'>
-                    <Image 
-                    className='min-w-[33px]'
-                    src="/govlogo.png"
-                    alt="Government Logo" 
-                    width={33}
-                    height={36}
-                    />
-                </a>
-            </li>
-            {items.map((item, index) => (
-                <NavbarItem key={index} title={item.title} url={item.url}/>
-            ))}
-        </ul>
-    </nav>
-    </header>
+    <nav className='text-[10pt] px-4 mx-auto'>
+    <ul className='flex items-center justify-center'>
+        <li className='px-4 transition-colors duration-200 cursor-pointer place-items-center hover:bg-primary-red'>
+            <a href='https://www.gov.ph/' className='py-[3px]'>
+                <Image 
+                className='min-w-[33px]'
+                src="/govlogo.png"
+                alt="Government Logo" 
+                width={33}
+                height={36}
+                />
+            </a>
+        </li>
+        {items.map((item, index) => (
+            <NavbarItem key={index} title={item.title} url={item.url}/>
+        ))}
+    </ul>
+</nav>
     )
 }
 
