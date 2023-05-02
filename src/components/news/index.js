a10020336c5407e9df73a4fb3c6c7df004bbca0d
Jaycee Go <itsmejcgo@gmail.com>
2023-05-02 20:06:54 +0800

    added red shade
    to do: add transition delay

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,16 +1,28 @@
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { ImFileText2 } from "react-icons/im";
+import Image from "next/image";
 
 function News() {
     return (
-        <div className="px-4 mb-7">  
-            <div className="article-title pb-[6px] mb-6 border-b-[1px] font-bold border-solid border-primary-light_dark">
-                <a href="#" className="pb-2 my-4 text-primary-red border-b-[1px] font-bold border-solid border-primary-red">Latest News from the University</a> 
-                <div className="buttons">
-                    
+        <div className="px-4 cursor-pointer mb-7">  
+            <div className="article-title flex items-center justify-between pb-[6px] mb-6 border-b-[1px] font-bold border-solid border-primary-light_dark">
+                <a href="#" className="pb-2 mb-4 text-primary-red border-b-[1px] font-bold border-solid border-primary-red">Latest News from the University</a> 
+                <div className="flex gap-1 mb-3 buttons">
+                    <div className="prev px-[2px] py-[1px] text-primary-low_dark">
+                        <MdOutlineKeyboardArrowLeft/>
+                    </div>
+                    <div className="next px-[2px] py-[1px] text-primary-low_dark">
+                        <MdOutlineKeyboardArrowRight/>
+                    </div>
                 </div>
             </div>
 
             <div className="news"> 
-                <div className="phivolcs"></div>
+                <div className="relative grid phivolcs place-items-center">
+                    <div className="absolute red-shade"></div>
+                    <ImFileText2 className="hidden w-auto h-12 mb-20 file-icon"/>
+                </div>
                 <div className="details">
                     <a href="#" className="text-primary-red ">
                         PHIVOLCS commits to training PUP on REDAS
