f691ce171d76c706ffe2ca27c583e91a4e8a51f3
Jaycee Go <itsmejcgo@gmail.com>
2023-05-04 00:37:41 +0800

    added quick links in footer (pup links) 2/3

--- src/components/pup-links/index.js
+++ src/components/pup-links/index.js
@@ -3,27 +3,52 @@ import FooterItem from "../footer-items";
 function PupLinks() {
   return (
     <div className="mx-auto">
-      <div className="service-download">
-        <div className="services mb-11">
+      <div className="flex">
+        {/* service and download */}
+        <div className="w-full lg:w-1/4 service-download">
+          <div className="services mb-11">
+            <FooterItem
+              title="ONLINE SERVICES"
+              items={[
+                "PUP WebMail",
+                "PUP iApply",
+                "SIS for Students",
+                "SIS for Faculty",
+              ]}
+            />
+          </div>
+          <div className="downloads">
+            <FooterItem
+              title="downloads"
+              items={["For Students", "For Faculty and Employees"]}
+            />
+          </div>
+        </div>
+        {/* quick links */}
+        <div className="w-full quick-links lg:w-1/4">
           <FooterItem
-            title="ONLINE SERVICES"
+            title="quick links"
             items={[
-              "PUP WebMail",
-              "PUP iApply",
-              "SIS for Students",
-              "SIS for Faculty",
+              "About PUP",
+              "Admission Information",
+              "Branches and Campuses",
+              "Academic Programs",
+              "Transparency Seal",
+              "Bid Notices and Invitation",
+              "International Affairs",
+              "Jobs for PUPians",
             ]}
           />
         </div>
-        <div className="downloads">
-          <FooterItem
-            title="downloads"
-            items={[
-              "For Students",
-              "For Faculty and Employees",
-            ]}
-          />
+        <div>
+          
         </div>
+      </div>
+      {/* copyright */}
+      <div>
+        <p className="text-xs text-center capitalize py-14 text-primary-gray">
+          © 1998-2021 Polytechnic University of the Philippines
+        </p>
       </div>
     </div>
   );
--- src/containers/footer/index.js
+++ src/containers/footer/index.js
@@ -2,11 +2,8 @@ import PupLinks from "@/components/pup-links";
 
 function FooterPup() {
   return (
-    <div className="container ">
-      <div className="pt-[60px] bg-primary-light">
-        <PupLinks />
-        
-      </div>
+    <div className="pt-[60px] container bg-primary-light">
+      <PupLinks />
     </div>
   );
 }
