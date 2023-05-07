2359fe87047636057f3a9884ee6650ecdcfcbb5a
Jaycee Go <itsmejcgo@gmail.com>
2023-05-07 21:51:39 +0800

    draft slider page added slide images, installed swiper
    to do: swiper navigation styling, pagination missing

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,17 +1,48 @@
-import Image from 'next/image';
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
-function Slider() {
+const imgSrc = [
+  "/slide.jpg",
+  "/slide 1.jpg",
+  "/slide 2.jpg",
+  "/slide 3.jpg",
+  "/slide 4.jpg",
+  "/slide 5.jpg",
+  "/slide 6.jpg",
+  "/slide 7.jpg",
+];
+
+function Slider(imgSwiper) {
   return (
-    <div className='relative'>
-        <Image 
-            className='w-full h-auto '
-            src="/slide.jpg"
-            alt="Slide 1" 
-            width={1800}
-            height={700}
-        />
+    <div className="relative">
+      <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        {imgSrc.map((src, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              className="w-full h-auto "
+              src={src}
+              alt="Slide 1"
+              width={1800}
+              height={700}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
